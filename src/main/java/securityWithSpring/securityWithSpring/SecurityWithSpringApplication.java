2cd767b591b584dfec397691e730f30c7057665f
Piotr Ludynia <pietrekpl18@gmail.com>
2020-11-13 17:53:50 +0100

    Initial commit

---
+++ src/main/java/securityWithSpring/securityWithSpring/SecurityWithSpringApplication.java
@@ -0,0 +1,13 @@
+package securityWithSpring.securityWithSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityWithSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityWithSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/securityWithSpring/securityWithSpring/SecurityWithSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package securityWithSpring.securityWithSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityWithSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
